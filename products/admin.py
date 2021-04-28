5d070935f8a52438b2a874155f25a1785077a321
Leeap83 <leeap83@gmail.com>
2021-04-28 15:32:27 +0000

    Products, Categories and Ingredients added to models.py and Database via admin

---
+++ products/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Product, Category, Ingredients
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Ingredients, IngredientsAdmin)
