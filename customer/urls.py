a360079a77f0d00dcfd7b1cc0ee6145003046036
Leeap83 <leeap83@gmail.com>
2021-04-30 11:07:28 +0000

    added suppliers.html page

--- customer/urls.py
+++ customer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.about_us, name='about_us'),
+    path('our_suppliers/', views.our_suppliers, name='our_suppliers'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -5,3 +5,9 @@ def about_us(request):
     """ A view to return about page """
 
     return render(request, 'customer/about.html')
+
+
+def our_suppliers(request):
+    """ A view to return about page """
+
+    return render(request, 'customer/suppliers.html')
