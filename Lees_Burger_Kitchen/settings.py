f178227d49720b240ae18032b0e0429a2035e38c
Leeap83 <leeap83@gmail.com>
2021-04-30 14:27:29 +0000

    added shopping cart.html

--- Lees_Burger_Kitchen/settings.py
+++ Lees_Burger_Kitchen/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'customer',
+    'cart',
 ]
 
 MIDDLEWARE = [
--- Lees_Burger_Kitchen/urls.py
+++ Lees_Burger_Kitchen/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('customer/', include('customer.urls')),
+    path('cart/', include('cart.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
