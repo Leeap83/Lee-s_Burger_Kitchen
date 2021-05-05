e45c45f123095900580d0dec1d566d3a0979a8d5
Leeap83 <leeap83@gmail.com>
2021-05-05 13:16:21 +0000

    Added signals.py, forms.py and admin to checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('name', 'email', 'phone_number',
+                  'street', 'town_or_city', 'postcode',
+                  'county',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postcode',
+            'town_or_city': 'Town or City',
+            'street': 'Street Address ',
+            'county': 'County',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
