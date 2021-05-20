bf48d3b216d89b269eee8dd91b1026128b5415e1
Leeap83 <leeap83@gmail.com>
2021-05-20 08:51:01 +0000

    Added new SECRET_KEY and set Debug to development in settings.py

--- Lees_Burger_Kitchen/settings.py
+++ Lees_Burger_Kitchen/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-6fj2%+1iqbj6wa+v!*kv!mjsoevu!#s*rjfp@54h#9pazs&%dd')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lees-burger-kitchen.herokuapp.com', 'localhost']
 
