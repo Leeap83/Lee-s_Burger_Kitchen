c06f9dbf90f6f44f4046f69f2901090574a59264
Leeap83 <leeap83@gmail.com>
2021-06-08 16:05:00 +0000

    made changes to custom_burger model & form

--- products/models.py
+++ products/models.py
@@ -47,23 +47,21 @@ class Ingredients(models.Model):
 class Custom_burger(models.Model):
 
     custom_name = models.CharField(max_length=254)
-    ingredients = models.ManyToManyField(
-        'Ingredients', related_name='ingredient', blank=True)
-    # buns = models.ManyToManyField(
-    #     'Ingredients', related_name='buns', blank=True)
-    # burger = models.ManyToManyField(
-    #     'Ingredients', related_name='burger', blank=True)
-    # sauce = models.ManyToManyField(
-    #     'Ingredients', related_name='sauce', blank=True)
-    # salads = models.ManyToManyField(
-    #     'Ingredients', related_name='salads', blank=True)
-    # cheese = models.ManyToManyField(
-    #     'Ingredients', related_name='cheese', blank=True)
-    # extras = models.ManyToManyField(
-    #     'Ingredients', related_name='extras', blank=True)
-    # category = models.ForeignKey(
-    #     'Category', null=True, blank=True, on_delete=models.CASCADE,
-    #     related_name='custom_burger')
+    buns = models.ManyToManyField(
+        'Ingredients', related_name='buns', blank=True)
+    burger = models.ManyToManyField(
+        'Ingredients', related_name='burger', blank=True)
+    sauce = models.ManyToManyField(
+        'Ingredients', related_name='sauce', blank=True)
+    salads = models.ManyToManyField(
+        'Ingredients', related_name='salads', blank=True)
+    cheese = models.ManyToManyField(
+        'Ingredients', related_name='cheese', blank=True)
+    extras = models.ManyToManyField(
+        'Ingredients', related_name='extras', blank=True)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.CASCADE,
+        related_name='custom_burger')
     price = models.DecimalField(
         max_digits=6, decimal_places=2, default='10.99', editable=False)
 
