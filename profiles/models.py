f12bffcc7242566120319e1695467365a7fe0979
Leeap83 <leeap83@gmail.com>
2021-06-09 14:16:01 +0000

    Uncommented out Userprofile

--- profiles/models.py
+++ profiles/models.py
@@ -20,12 +20,12 @@ class UserProfile(models.Model):
         return f'{self.user.username} Profile'
 
 
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     """
-#     Create or update the user profile
-#     """
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     # Existing users: just save the profile
-#     instance.userprofile.save()
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
